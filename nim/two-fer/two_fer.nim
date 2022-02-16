84353eaef64d1620160a02e8cf3a6935b30087fa
NotSmurf1245 <trace.davis2020@outlook.com>
2022-02-16 15:44:58 -0600

    master

--- nim/two-fer/two_fer.nim
+++ nim/two-fer/two_fer.nim
@@ -1,5 +1,5 @@
 proc twoFer(name: string) =
     if name.len > 0:
-        echo "One for {name}, one for me."
+        fmt"One for {name}, one for me."
     else:
-        echo "One for you, one for me"
+        "One for you, one for me"
